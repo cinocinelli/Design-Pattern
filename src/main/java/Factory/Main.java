e3ad5652ad47688e18ee6484cbe76288edd76b19
han <563172032@qq.com>
2024-04-22 22:20:55 +0800

    delete Factory Main.java

--- src/main/java/Factory/Main.java
+++
@@ -1,9 +0,0 @@
-package Factory;
-
-public class Main {
-    public static void main(String[] args) {
-        Factory shapeFactory = new Factory();
-        Shape shape = shapeFactory.getShape(1);
-        shape.draw();
-    }
-}
